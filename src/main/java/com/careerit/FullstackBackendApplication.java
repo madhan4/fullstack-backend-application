54c68df3af1af6d6b064c6b1da6c96efe5bbb5e1
madhan4 <madhangoud4@gmail.com>
2026-04-16 20:08:03 -0700

    Removed caching config - fixing startup issue

--- src/main/java/com/careerit/FullstackBackendApplication.java
+++ src/main/java/com/careerit/FullstackBackendApplication.java
@@ -2,10 +2,8 @@ package com.careerit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class FullstackBackendApplication {
 
 	public static void main(String[] args) {
